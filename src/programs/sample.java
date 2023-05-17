7fe8b96a02847368512ad2a1aff6f5edd71c8b30
admin <admin@DESKTOP-AJGSUDM>
2023-05-17 11:24:34 +0530

    This is my first commit

---
+++ src/programs/sample.java
@@ -0,0 +1,12 @@
+package programs;
+
+public class sample {
+
+	public static void main(String[] args) {
+		if (true)
+		{
+			System.out.println("this is a code present in master");
+		}
+			
+}
+}
